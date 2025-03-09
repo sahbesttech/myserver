64b18bb35592d683d648fd12f7f6a906ce0a65ea
Saheed Adeniyi Adebayo <sahbesttech1@gmail.com>
2025-03-09 12:20:03 +0100

    did a little renaming

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const ActivationCode = require('./models/ActivationCode');
+const ActivationCode = require('../models/ActivationCode');
 
 
 const app = express();
